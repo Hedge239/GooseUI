687df2c58785859f854dbd606b06e993ac269ed0
Lucas Kohorst <70863326+Hedge239@users.noreply.github.com>
2025-03-30 22:18:12 -0400

    Updated inital code for X11
    Not bother putting a update log, only minor changes

---
+++ src/zPlatform/x11/zX11_zWindow.cpp
@@ -0,0 +1,37 @@
+#include "ZephyrUI/zPlatform/x11/zX11_zWindow.h"
+#include <X11/Xlib.h>
+
+using namespace zUI;
+using namespace zPlatform;
+using namespace zX11;
+
+zX11_zWindow::zX11_zWindow(const std::string& title, zUI::zCore::zEnumerations::zWindowPos windowPos, int width, int height)
+{
+}
+
+zX11_zWindow::~zX11_zWindow()
+{
+    XDestroyWindow(_display, _window);
+    XCloseDisplay(_display);
+}
+
+// Window Settings
+void zX11_zWindow::setTitle(const std::string& title) {}
+void zX11_zWindow::setSize(int width, int height) {}
+
+// Returns
+int zX11_zWindow::getPosX() {return 0;}
+int zX11_zWindow::getPosY() {return 0;}
+int zX11_zWindow::getWidth() {return 0;}
+int zX11_zWindow::getHeight() {return 0;}
+
+// Window Management
+void zX11_zWindow::show() {}
+
+void zX11_zWindow::hide() {}
+void zX11_zWindow::destroy() {}
+void zX11_zWindow::draw() {}
+
+void zX11_zWindow::HandelEvents()
+{
+}
