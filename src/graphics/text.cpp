843b733d5a9e7a0d0c785aaf367da95f73ba2b18
Lucas Kohorst <70863326+Hedge239@users.noreply.github.com>
2025-10-11 13:26:35 -0400

    Fixed quad rendering
    Now I must figureout blending

--- src/graphics/text.cpp
+++ src/graphics/text.cpp
@@ -4,7 +4,7 @@
 
 #include "GooseUI/core/templates/renderBase.h"
 
-#include <iostream>
+
 namespace goose::graphics
 {
     void text::draw(core::templates::renderBase::renderer& renderer, graphics::font::font* font, const std::string& text, int X, int Y, const core::templates::renderBase::color& color)
@@ -18,7 +18,25 @@ namespace goose::graphics
         for(const auto& gp : layout.getGlyphs())
         {
             if (gp.glyph->width == 0 || gp.glyph->height == 0) { continue; }
-            renderer.drawTextureQuad(atlas.getID(), X, Y, gp.glyph->width, gp.glyph->height, gp.glyph->u0, gp.glyph->u1, gp.glyph->v0, gp.glyph->v1, color);
+
+            float u0 = gp.glyph->u0;
+            float v0 = gp.glyph->v0;
+            float u1 = gp.glyph->u1;
+            float v1 = gp.glyph->v1;
+
+            v0 = 1.0f - v0;
+            v1 = 1.0f - v1;
+            std::swap(v0, v1);
+
+            float xpos = X + gp.glyph->xOffset;
+            float ypos = Y - (gp.glyph->height - gp.glyph->yOffset);
+            float w = gp.glyph->width;
+            float h = gp.glyph->height;
+
+            //renderer.drawTextureQuad(atlas.getID(), X, Y, gp.glyph->width, gp.glyph->height, gp.glyph->u0, gp.glyph->u1, gp.glyph->v0, gp.glyph->v1, color);
+            renderer.drawTextureQuad(atlas.getID(), X, Y, gp.glyph->width, gp.glyph->height, u0, v0, u1, v1, color);
+
+            X += (gp.glyph->xAdvance);
         }
     }
 }
