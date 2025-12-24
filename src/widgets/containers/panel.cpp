3a08e702e25737351d6fcfd685e110614f45956d
Lucas Kohorst <70863326+Hedge239@users.noreply.github.com>
2025-12-23 21:02:36 -0500

    Updated Panel color system & fix test

--- src/widgets/containers/panel.cpp
+++ src/widgets/containers/panel.cpp
@@ -15,62 +15,65 @@ namespace goose::widgets::containers
         _height = Height;
         
         _isVisible = true;
-        _color = { 0.0f, 0.0f, 0.0f, 1.0f };
+        _color = { 0.85f, 0.85f, 0.85f, 1.0f };
     }
     
     panel* createPanel(core::types::componentScale componentScaleing, int componentAlign, int X, int Y, int Width, int Height)
         { return new panel(componentScaleing, componentAlign, X, Y, Width, Height); }
-        
-        // overides
-        void panel::draw(interface::iRenderer& renderer)
-        {
-            if(!_isVisible || !_hostWindow){ return; }
-            if(_hostParent){ _isVisible = _hostParent->isVisible(); }
-            
-            // pain
-            const int hostWidth = _hostParent ? _hostParent->getWidth() : _hostWindow->getWidth(); const int hostHeight = _hostParent ? _hostParent->getHeight() : _hostWindow->getHeight();
-            graphics::layout::calculator::calculateLayout(_scaleing, _alignment, _sizeRestraints, _initalBounds, hostWidth, hostHeight, _posX, _posY, _width, _height);
-            
-            renderer.drawRect(_posX, _posY, _width, _height, _color);
-        }
-        
-        void panel::pollEvent(core::types::event::eventData evtData){ return; }
-        
-        // If a widget has a parent, we can not add or remove it from a window unless the parent is also removed - vise vera if no parent but attached to window, cannot add parent 
-        void panel::addToWindow(interface::iWindow* window)
-        {
-            if(!window || _hostParent){ return; }
-            _hostWindow = window;
-            _hostWindow->addWidgetToVector(this);
-            graphics::layout::calculator::getInitalOffsets(_initalBounds, _hostWindow->getWidth(), _hostWindow->getHeight(), _posX, _posY, _width, _height);
-        }
-        
-        void panel::removeFromWindow()
-        {
-            if(!_hostWindow || _hostParent){ return; }
-            _hostWindow->removeWidgetFromVector(this);
-            _hostWindow = nullptr;
-        }
-        
-        void panel::setParent(iWidget* widget)
-        { 
-            if(!widget || _hostWindow){ return; }
-            _hostParent = widget; _hostWindow = _hostParent->getWindow();
-            _hostWindow->addWidgetToVector(this);
-            graphics::layout::calculator::getInitalOffsets(_initalBounds, _hostParent->getWidth(), _hostParent->getHeight(), _posX, _posY, _width, _height);
-        }
-        
-        void panel::removeParent()
-        { 
-            if(!_hostParent){ return; }
-            _hostWindow->removeWidgetFromVector(this);
-            _hostWindow = nullptr; _hostParent = nullptr; 
-        }
     
-        void panel::show() { _isVisible = true; }
-        void panel::hide() { _isVisible = false; }
+        
+    void panel::setColor(const core::types::color& color){ _color = color; }
     
-        void panel::setSize(int width, int height) { _width = width; _height = height;}
-        void panel::setSizeRestraints(int minWidth, int minHeight, int maxWidth, int maxHeight) { _sizeRestraints.minWidth = minWidth; _sizeRestraints.minHeight = minHeight; _sizeRestraints.maxWidth = maxWidth; _sizeRestraints.maxHeight = maxHeight; }
-        void panel::setPosistion(int X, int Y) { _posX = X; _posY = Y; }
+    // overides
+    void panel::draw(interface::iRenderer& renderer)
+    {
+        if(!_isVisible || !_hostWindow){ return; }
+        if(_hostParent){ _isVisible = _hostParent->isVisible(); }
+        
+        // pain
+        const int hostWidth = _hostParent ? _hostParent->getWidth() : _hostWindow->getWidth(); const int hostHeight = _hostParent ? _hostParent->getHeight() : _hostWindow->getHeight();
+        graphics::layout::calculator::calculateLayout(_scaleing, _alignment, _sizeRestraints, _initalBounds, hostWidth, hostHeight, _posX, _posY, _width, _height);
+        
+        renderer.drawRect(_posX, _posY, _width, _height, _color);
+    }
+        
+    void panel::pollEvent(core::types::event::eventData evtData){ return; }
+        
+    // If a widget has a parent, we can not add or remove it from a window unless the parent is also removed - vise vera if no parent but attached to window, cannot add parent 
+    void panel::addToWindow(interface::iWindow* window)
+    {
+        if(!window || _hostParent){ return; }
+        _hostWindow = window;
+        _hostWindow->addWidgetToVector(this);
+        graphics::layout::calculator::getInitalOffsets(_initalBounds, _hostWindow->getWidth(), _hostWindow->getHeight(), _posX, _posY, _width, _height);
+    }
+        
+    void panel::removeFromWindow()
+    {
+        if(!_hostWindow || _hostParent){ return; }
+        _hostWindow->removeWidgetFromVector(this);
+        _hostWindow = nullptr;
+    }
+        
+    void panel::setParent(iWidget* widget)
+    { 
+        if(!widget || _hostWindow){ return; }
+        _hostParent = widget; _hostWindow = _hostParent->getWindow();
+        _hostWindow->addWidgetToVector(this);
+        graphics::layout::calculator::getInitalOffsets(_initalBounds, _hostParent->getWidth(), _hostParent->getHeight(), _posX, _posY, _width, _height);
+    }
+        
+    void panel::removeParent()
+    { 
+        if(!_hostParent){ return; }
+        _hostWindow->removeWidgetFromVector(this);
+        _hostWindow = nullptr; _hostParent = nullptr; 
+    }
+    
+    void panel::show() { _isVisible = true; }
+    void panel::hide() { _isVisible = false; }
+    
+    void panel::setSize(int width, int height) { _width = width; _height = height;}
+    void panel::setSizeRestraints(int minWidth, int minHeight, int maxWidth, int maxHeight) { _sizeRestraints.minWidth = minWidth; _sizeRestraints.minHeight = minHeight; _sizeRestraints.maxWidth = maxWidth; _sizeRestraints.maxHeight = maxHeight; }
+    void panel::setPosistion(int X, int Y) { _posX = X; _posY = Y; }
 }
--- tests/ParentTest.cpp
+++ tests/ParentTest.cpp
@@ -1,4 +1,44 @@
+#include "GooseUI/GooseUI.h"
+
+#include "GooseUI/widgets/containers/panel.h"
+#include "GooseUI/widgets/base/window.h"
+
 int main()
 {
+    goose::interface::iWindow* window1;
     
+    goose::widgets::containers::panel* panel1;
+    goose::widgets::containers::panel* panel2;
+    
+    window1 = goose::widgets::base::createWindow(
+        "MyWindow",
+        500, 500,
+        goose::core::types::windowPosistion::SCREEN_CENTER,
+        goose::core::types::graphicsBackend::opengl
+    );
+    
+    window1->setHeader("ParentTest", true, true, true, true);
+    window1->isResizeable(true);
+    
+    panel1 = goose::widgets::containers::createPanel(
+        goose::core::types::componentScale::SCALE_ALL, 
+        goose::core::types::componentAlign::ALIGN_LEFT | goose::core::types::componentAlign::ALIGN_TOP | goose::core::types::componentAlign::ALIGN_RIGHT | goose::core::types::componentAlign::ALIGN_BOTTOM, 
+        10, 10, 
+        400, 400
+    );
+    
+    panel2 = goose::widgets::containers::createPanel(
+        goose::core::types::componentScale::SCALE_NONE, 
+        goose::core::types::componentAlign::ALIGN_LEFT | goose::core::types::componentAlign::ALIGN_TOP | goose::core::types::componentAlign::ALIGN_RIGHT | goose::core::types::componentAlign::ALIGN_BOTTOM, 
+        200, 200, 
+        20, 20
+    );
+    
+    panel2->setColor({1, 0, 0, 1});
+    
+    panel1->addToWindow(window1);
+    panel2->setParent(panel1);
+    
+    window1->show();
+    goose::core::eventloop::run({window1});
 }
